59bfe7b8cfa25a75b4a79e6cccc759182dcfe109
Abhirama05 <rama.durant05@gmail.com>
2023-12-07 23:15:00 +0700

    first commit

---
+++ _1_helloWorld.java
@@ -0,0 +1,11 @@
+package belajar_java_CS;
+
+public class _1_helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("Hello World");
+        System.out.print("Hello ");
+        System.out.print("Wolrd");
+    }
+}
